afcbdb47d4694a68960935e7c75c7402e56d3322
annamaivy <anna.maivy@gmail.com>
2017-12-21 01:30:59 +0100

    All methods working except history

--- js/create.js
+++ js/create.js
@@ -10,7 +10,7 @@ $(document).ready(() => {
         SDK.User.createUser(username, password, (err, data) => {
             console.log(err, data);
             if (err){
-                console.log("Error - Could not create user")
+                console.log("Error - Could not create the user")
             } else {
                 console.log(data);
                 window.location.href = "login.html";
